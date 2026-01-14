ca245d3b480ff55c3a67799fcd11cbbcd3e68b78
Darby Johnston <darbyjohnston@yahoo.com>
2026-01-13 16:25:24 -0800

    Update tlRender version

--- etc/SuperBuild/cmake/Modules/BuildtlRender.cmake
+++ etc/SuperBuild/cmake/Modules/BuildtlRender.cmake
@@ -1,7 +1,7 @@
 include(ExternalProject)
 
 set(TLRENDER_GIT_REPOSITORY "https://github.com/grizzlypeak3d/tlRender.git")
-set(TLRENDER_GIT_TAG "57d8d51603a93445e484e93cd03e1c23268147a0")
+set(TLRENDER_GIT_TAG "33ad4203dfc82fa9689825e8551e6dae285c72b8")
 
 set(TLRENDER_DEPS)
 set(TLRENDER_ARGS
