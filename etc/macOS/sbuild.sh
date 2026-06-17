0a556824a79b9cc33383d2b7a5885d5fb093b193
Darby Johnston <darbyjohnston@yahoo.com>
2026-06-17 10:56:12 -0700

    Shorten paths

--- etc/macOS/sbuild.sh
+++ etc/macOS/sbuild.sh
@@ -9,7 +9,7 @@ git -C $SOURCE_DIR submodule update --init --recursive
 
 cmake \
     -S $SOURCE_DIR/deps/tlRender/deps/ftk/etc/SuperBuild \
-    -B sbuild-ftk-$BUILD_TYPE \
+    -B ftk-$BUILD_TYPE \
     -Dftk_PYTHON=$TLRENDER_PYTHON \
     -DBUILD_SHARED_LIBS=$BUILD_SHARED_LIBS \
     -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
@@ -17,11 +17,11 @@ cmake \
     -DCMAKE_PREFIX_PATH=$PWD/install-$BUILD_TYPE \
     -DCMAKE_OSX_DEPLOYMENT_TARGET=$CMAKE_OSX_DEPLOYMENT_TARGET \
     -DCMAKE_OSX_ARCHITECTURES=$CMAKE_OSX_ARCHITECTURES
-cmake --build sbuild-ftk-$BUILD_TYPE -j $JOBS --config $BUILD_TYPE
+cmake --build ftk-$BUILD_TYPE -j $JOBS --config $BUILD_TYPE
 
 cmake \
     -S $SOURCE_DIR/deps/tlRender/etc/SuperBuild \
-    -B sbuild-tl-$BUILD_TYPE \
+    -B tl-$BUILD_TYPE \
     -DTLRENDER_NET=$TLRENDER_NET \
     -DTLRENDER_OCIO=$TLRENDER_OCIO \
     -DTLRENDER_JPEG=$TLRENDER_JPEG \
@@ -44,7 +44,7 @@ cmake \
     -DCMAKE_PREFIX_PATH=$PWD/install-$BUILD_TYPE \
     -DCMAKE_OSX_DEPLOYMENT_TARGET=$CMAKE_OSX_DEPLOYMENT_TARGET \
     -DCMAKE_OSX_ARCHITECTURES=$CMAKE_OSX_ARCHITECTURES
-cmake --build sbuild-tl-$BUILD_TYPE -j $JOBS --config $BUILD_TYPE
+cmake --build tl-$BUILD_TYPE -j $JOBS --config $BUILD_TYPE
 
 cmake \
     -S $SOURCE_DIR \
