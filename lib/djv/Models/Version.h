0fda64b385e25c8d49ad41646eb296e96e3d858a
Darby Johnston <darbyjohnston@yahoo.com>
2026-08-18 15:10:47 -0700

    Bump version

--- lib/djv/Models/Version.h
+++ lib/djv/Models/Version.h
@@ -4,7 +4,7 @@
 #pragma once
 
 #define DJV_VERSION_MAJOR 3
-#define DJV_VERSION_MINOR 5
-#define DJV_VERSION_PATCH 1
+#define DJV_VERSION_MINOR 6
+#define DJV_VERSION_PATCH 0
 #define DJV_VERSION_DEV "-dev"
 #define DJV_VERSION_FULL "3.5.1-dev"
