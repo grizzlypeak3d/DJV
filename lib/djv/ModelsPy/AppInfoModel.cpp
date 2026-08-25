718c3f2a9269d1a2db30762c065645f20ba55209
Darby Johnston <darbyjohnston@yahoo.com>
2026-08-25 10:11:39 -0700

    Python applications can say who they are
    The application information model's name getters are virtual so that
    an application built on DJV can override them, which the studio suite
    does; the trampoline lets a Python application do the same.

--- lib/djv/ModelsPy/AppInfoModel.cpp
+++ lib/djv/ModelsPy/AppInfoModel.cpp
@@ -13,12 +13,39 @@ namespace djv
 {
     namespace python
     {
+        //! The name getters are virtual so that an application built on DJV
+        //! can say who it is -- the trampoline lets a Python application do
+        //! the same.
+        class PyAppInfoModel : public models::AppInfoModel
+        {
+        public:
+            static std::shared_ptr<PyAppInfoModel> create()
+            {
+                return std::shared_ptr<PyAppInfoModel>(new PyAppInfoModel);
+            }
+
+            std::string getFullName() const override
+            {
+                PYBIND11_OVERRIDE(std::string, models::AppInfoModel, getFullName);
+            }
+
+            std::string getShortName() const override
+            {
+                PYBIND11_OVERRIDE(std::string, models::AppInfoModel, getShortName);
+            }
+
+            std::string getDocsDirName() const override
+            {
+                PYBIND11_OVERRIDE(std::string, models::AppInfoModel, getDocsDirName);
+            }
+        };
+
         void appInfoModel(py::module_& m)
         {
             using namespace models;
 
-            py::class_<AppInfoModel, std::shared_ptr<AppInfoModel> >(m, "AppInfoModel")
-                .def(py::init(&AppInfoModel::create))
+            py::class_<AppInfoModel, std::shared_ptr<AppInfoModel>, PyAppInfoModel>(m, "AppInfoModel")
+                .def(py::init(&PyAppInfoModel::create))
 
                 .def_property_readonly("fullName", &AppInfoModel::getFullName)
                 .def_property_readonly("shortName", &AppInfoModel::getShortName)
