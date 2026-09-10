695607e15700ee1b7287bb5a188fa51733f49369
Darby Johnston <darbyjohnston@yahoo.com>
2026-09-09 17:17:04 -0700

    The system build installs the OpenImageIO tools
    The library's CMake configuration names them as targets and refuses to
    configure without the files.

--- etc/Linux/setup-gha-system.sh
+++ etc/Linux/setup-gha-system.sh
@@ -10,5 +10,5 @@ set -x
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get install -y build-essential git cmake xorg-dev libglu1-mesa-dev mesa-common-dev mesa-utils libasound2-dev libpulse-dev libva-dev libdrm-dev libwayland-dev wayland-protocols libxkbcommon-dev libegl1-mesa-dev libdecor-0-dev pkg-config zlib1g-dev libpng-dev libfreetype-dev nlohmann-json3-dev libsdl3-dev libimath-dev libopenexr-dev libopencolorio-dev libopenimageio-dev libopentimelineio-dev libminizip-ng-dev libavcodec-dev libavdevice-dev libavformat-dev libavutil-dev libswresample-dev libswscale-dev xvfb libgl1-mesa-dri
+apt-get install -y build-essential git cmake xorg-dev libglu1-mesa-dev mesa-common-dev mesa-utils libasound2-dev libpulse-dev libva-dev libdrm-dev libwayland-dev wayland-protocols libxkbcommon-dev libegl1-mesa-dev libdecor-0-dev pkg-config zlib1g-dev libpng-dev libfreetype-dev nlohmann-json3-dev libsdl3-dev libimath-dev libopenexr-dev libopencolorio-dev libopenimageio-dev openimageio-tools libopentimelineio-dev libminizip-ng-dev libavcodec-dev libavdevice-dev libavformat-dev libavutil-dev libswresample-dev libswscale-dev xvfb libgl1-mesa-dri
 xvfb-run glxinfo
