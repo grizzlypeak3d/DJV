f739a4cf5574fa7d91c6b5856ffdf3bd19f3a327
Darby Johnston <darbyjohnston@yahoo.com>
2026-09-18 17:05:02 -0700

    The bindings use feather-tk's std::function caster
    nanobind's can't be included beside it, and DJV's Python build broke on
    the redefinition.

--- lib/djv/ModelsPy/AudioModel.cpp
+++ lib/djv/ModelsPy/AudioModel.cpp
@@ -14,7 +14,7 @@
 #include <ftk/UI/Settings.h>
 #include <ftk/Core/Context.h>
 
-#include <nanobind/stl/function.h>
+#include <ftk/CorePy/Function.h>
 #include <nanobind/stl/string.h>
 #include <nanobind/stl/vector.h>
 #include <nanobind/stl/list.h>
--- lib/djv/ModelsPy/OCIOModel.cpp
+++ lib/djv/ModelsPy/OCIOModel.cpp
@@ -13,7 +13,7 @@
 #include <ftk/CorePy/Bindings.h>
 #include <ftk/Core/Context.h>
 
-#include <nanobind/stl/function.h>
+#include <ftk/CorePy/Function.h>
 #include <nanobind/operators.h>
 #include <nanobind/stl/string.h>
 #include <nanobind/stl/vector.h>
